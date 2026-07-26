9777a4592d62f8acb94618e8f68cbeddd1bfc2f3
Jahmai Cabrera <127389693+Fresht0Death@users.noreply.github.com>
2026-07-25 17:22:12 -0700

    Refactor ADC functions for error handling and voltage conversion

--- drivers/src/adc.c
+++ drivers/src/adc.c
@@ -11,6 +11,7 @@
 #include "stm32f4xx.h"
 #include <stdint.h>
 #include "timer.h"
+#include "stm_err.h"
 /* /\ End of Libraries /\ */
 
 /* \/ Start of Macros \/ */
@@ -40,12 +41,12 @@ volatile uint32_t numSamples = 0;
  * 										*
  * Name: single_polling_adc1_pa0_init	*
  *  @args: void							*
- *  @param: void						*
+ *  @param: my_err_t to check status	*
  *  @brief: initialize adc1 via pa0 for *
  *  single polling						*
  * 										*
  *======================================*/
-void single_polling_adc1_pa0_init(void)
+my_err_t single_polling_adc1_pa0_init(void)
 {
 	// Enable clock access to ADC1
 	RCC->APB2ENR |=	ADC1EN;
@@ -84,6 +85,8 @@ void single_polling_adc1_pa0_init(void)
 	// short stabilization delay
 	for(int i =0; i <10000; i++);
 
+	return MY_OK;
+
 }
 
 
@@ -91,7 +94,7 @@ void single_polling_adc1_pa0_init(void)
  * 											*
  * Name: read_adc1_pa0_value				*
  *  @args: void								*
- *  @param: uint32_t that returns adc value	*					*
+ *  @param: uint32_t that returns adc value	*
  *  @brief: start conversion and wait until *
  *  analog to digital conversion complete	*
  *  										*
@@ -111,34 +114,103 @@ uint32_t read_adc1_pa0_value(void)
 }
 
 
+/*==========================================*
+ * 											*
+ * Name: adc_to_voltage3V3_integer_part		*
+ *  @args: uint32_t adc_value				*
+ *  @param: uint32_t whole number part of   *
+ *  					of voltage	        *
+ *  @brief: convert adc to voltage when 	*
+ *  		connected to 3.3V and returns 	*
+ *  		the whole number part of the	*
+ *  		voltage							*
+ *  										*
+ *==========================================*/
 uint32_t adc_to_voltage3V3_integer_part(uint32_t adc_value)
 {
 
-	return ((adc_value * 5)/(4096));
+	return ((adc_value * 3300U)/(4096U));
 
 }
 
 
-uint32_t adc_to_voltage5V_integer_part(uint32_t adc_value)
+/*==========================================*
+ * 											*
+ * Name: adc_to_voltage3V3_decimal_part		*
+ *  @args: uint32_t adc_value				*
+ *  @param: uint32_t fraction part of       *
+ *  					of voltage	        *
+ *  @brief: convert adc to voltage when 	*
+ *  		connected to 3.3V and returns 	*
+ *  		the fraction part of the	    *
+ *  		voltage							*
+ *  										*
+ *==========================================*/
+uint32_t adc_to_voltage3V3_decimal_part(uint32_t adc_value)
 {
 
-	return ((adc_value * 5)/(4096));
+	uint32_t remainder = ((adc_value * 3300U)%(4096U));
 
-}
-
-uint32_t adc_to_voltage5V_decimal_part(uint32_t adc_value)
-{
-
-	uint32_t remainder = ((adc_value * 5)%(4096));
-
-	uint32_t decimal_part = (remainder * 1000) / 4096;
+	uint32_t decimal_part = (remainder * 1000U) / 4096U;
 
 	return decimal_part;
 
+}
 
+
+/*==========================================*
+ * 											*
+ * Name: adc_to_voltage5V_integer_part		*
+ *  @args: uint32_t adc_value				*
+ *  @param: uint32_t whole number part of   *
+ *  					of voltage	        *
+ *  @brief: convert adc to voltage when 	*
+ *  		connected to 5V and returns 	*
+ *  		the whole number part of the	*
+ *  		voltage							*
+ *  										*
+ *==========================================*/
+uint32_t adc_to_voltage5V_integer_part(uint32_t adc_value)
+{
+
+	return ((adc_value * 5U)/(4096U));
 
 }
 
+
+/*==========================================*
+ * 											*
+ * Name: adc_to_voltage5V_decimal_part		*
+ *  @args: uint32_t adc_value				*
+ *  @param: uint32_t fraction part of       *
+ *  					of voltage	        *
+ *  @brief: convert adc to voltage when 	*
+ *  		connected to 5V and returns 	*
+ *  		the fraction part of the	    *
+ *  		voltage							*
+ *  										*
+ *==========================================*/
+uint32_t adc_to_voltage5V_decimal_part(uint32_t adc_value)
+{
+
+	uint32_t remainder = ((adc_value * 5U)%(4096U));
+
+	uint32_t decimal_part = (remainder * 1000U) / 4096U;
+
+	return decimal_part;
+
+}
+
+
+/*==========================================*
+ * 											*
+ * Name: adc1_pa0_avg_voltage				*
+ *  @args: uint32_t sample					*
+ *  @param: uint32_t avg of adc1 samples    *
+ *  @brief: calculates the avg voltage of   *
+ *  		the adc1 values via pa0			*
+ *  										*
+ *==========================================*/
 uint32_t adc1_pa0_avg_voltage(uint32_t sample)
 {
 	sumSamples = sample + sumSamples;
@@ -148,13 +220,32 @@ uint32_t adc1_pa0_avg_voltage(uint32_t sample)
 }
 
 
-
+/*==========================================*
+ * 											*
+ * Name: num_of_samples						*
+ *  @args: void								*
+ *  @param: uint32_t number of samples      *
+ *  @brief: returns current number of 		*
+ *  		samples that have been 			*
+ *  		calculated by adc1				*
+ *  										*
+ *==========================================*/
 uint32_t num_of_samples(void)
 {
 	return numSamples;
 }
 
-void adc2_pa4_interrupt_oneshot_int(void)
+
+/*==========================================*
+ * 											*
+ * Name: adc2_pa4_interrupt_oneshot_init	*
+ *  @args: void								*
+ *  @param: void							*
+ *  @brief: initialize adc2 via pa4 	 	*
+ *  		to do one adc cycle				*
+ * 											*
+ *==========================================*/
+void adc2_pa4_interrupt_oneshot_init(void)
 {
 
 	// Enable clock access to ADC2
@@ -200,6 +291,17 @@ void adc2_pa4_interrupt_oneshot_int(void)
 
 }
 
+
+/*==========================================*
+ * 											*
+ * Name: adc2_pa4_interrupt_continuous_init	*
+ *  @args: void								*
+ *  @param: void							*
+ *  @brief: initialize adc2 via pa4 for 	*
+ *  		to be continuously converting	*
+ *  		values through an interrupt		*
+ * 											*
+ *==========================================*/
 void adc2_pa4_interrupt_continuous_init(void)
 {
 	// Enable clock access to ADC2
@@ -246,6 +348,18 @@ void adc2_pa4_interrupt_continuous_init(void)
 
 }
 
+
+/*==========================================*
+ * 											*
+ * Name: adc1_pa0_single_polling_timed_init	*
+ *  @args: uint32_t, TimerHandle*								*
+ *  @param: void							*
+ *  @brief: initialize adc1 via pa0 for 	*
+ *  		to return its based on the 		*
+ *  		timed sequence chosen by the	*
+ *  		user							*
+ * 											*
+ *==========================================*/
 void adc1_pa0_single_polling_timed_init(uint32_t delayMs, TimerHandle *timer)
 {
 	single_polling_adc1_pa0_init();
@@ -256,4 +370,3 @@ void adc1_pa0_single_polling_timed_init(uint32_t delayMs, TimerHandle *timer)
 
 }
 /* /\ End of function definitions /\ */
-
