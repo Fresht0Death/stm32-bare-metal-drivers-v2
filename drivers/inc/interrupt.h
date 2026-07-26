e19a7e7f28037d4f39873961f935a307708b2ec9
Jahmai Cabrera <127389693+Fresht0Death@users.noreply.github.com>
2026-07-25 17:19:35 -0700

    Add interrupt initialization function declarations
    Added function declarations for user button and timer interrupts.

--- drivers/inc/interrupt.h
+++ drivers/inc/interrupt.h
@@ -10,8 +10,40 @@
 
 #include <stdint.h>
 
+
+/*======================================*
+ * 										*
+ *  Name: pc13_user_button_exti_init  	*
+ *   @param: N/A						*
+ *   @brief: start user button B1		*
+ *   interrupt							*
+ *   @return: N/A						*
+ * 										*
+ *======================================*/
 void pc13_user_button_exti_init(void);
+
+
+/*==========================================*
+ * 											*
+ *  Name: tim2_periodic_interrupt_init_ms   *
+ *   @param: durationMs takes the			*
+ *   number of milliseconds the				*
+ *   user wants to time						*
+ *   @brief: start tim2 interrupt			*
+ *   @return: N/A							*
+ * 											*
+ *==========================================*/
 void tim2_periodic_interrupt_init_ms(uint32_t durationMs);
+
+
+/*======================================*
+ * 										*
+ *  Name: usart2_interrupt_int	    	*
+ *   @param: N/A						*
+ *   @brief: enable usart2 rx interrupt	*
+ *   @return: N/A						*
+ * 										*
+ *======================================*/
 void usart2_interrupt_init(void);
 
 
