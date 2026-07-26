cc28484dffbc4d9a550d77505e8d43f289865e13
Jahmai Cabrera <127389693+Fresht0Death@users.noreply.github.com>
2026-07-25 17:19:07 -0700

    Add GPIO functions and remove unused delay function

--- drivers/inc/gpio.h
+++ drivers/inc/gpio.h
@@ -8,10 +8,55 @@
 #ifndef GPIO_H_
 #define GPIO_H_
 
+#include <stdint.h>
+
+
+/*======================================*
+ * 										*
+ * Name: toggle_pa5_led					*
+ *  @args: void							*
+ *  @param: void						*
+ *  @brief: toggles the on-board led		*
+ * 										*
+ *======================================*/
 void toggle_pa5_led(void);
+
+
+/*======================================*
+ * 										*
+ * Name: gpioa_set_mode					*
+ *  @args: uint8_t, uint8_t				*
+ *  @param: void						*
+ *  @brief: set the mode of any pin		*
+ *  		in GPIOA					*
+ * 										*
+ *======================================*/
 void gpioa_set_mode(uint8_t mode, uint8_t pin);
+
+
+/*======================================*
+ * 										*
+ * Name: gpioa_enable					*
+ *  @args: void							*
+ *  @param: void						*
+ *  @brief: enable clock access to 		*
+ *  		GPIOA						*
+ * 										*
+ *======================================*/
 void gpioa_enable(void);
-void shit_delay(void);
+
+
+/*======================================*
+ * 										*
+ * Name: delay							*
+ *  @args: void							*
+ *  @param: void						*
+ *  @brief: starts a blocking delay		*
+ * 										*
+ *======================================*/
+void delay(void);
+
+
 #define GPIOAEN		    (1U<<0)
 #define PA5				(1U<<5)
 
